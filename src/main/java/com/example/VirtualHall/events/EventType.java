675d3f9abc131700ac17e1a7f7f5bdcc8c85bbeb
armen <armenpetrosan319@gmail.com>
2026-02-16 08:50:10 +0500

    Реализовал сущность Event без валидации и возможности добавления

---
+++ src/main/java/com/example/VirtualHall/events/EventType.java
@@ -0,0 +1,16 @@
+package com.example.VirtualHall.events;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "eventType")
+public class EventType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eventTypeId;
+
+    @Column(name = "name", nullable = false, unique = true, length = 70) // 70 размер
+    private String name;
+
+}
