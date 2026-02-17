49597ad02fdfce65d394c8fb81f9efdad476b807
armen <armenpetrosan319@gmail.com>
2026-02-17 11:38:48 +0500

    Реализован просмотр мероприятий

--- src/main/java/com/example/VirtualHall/events/EventController.java
+++ src/main/java/com/example/VirtualHall/events/EventController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/events")
@@ -37,5 +40,17 @@ public class EventController {
         model.addAttribute("selectedEventTypeIds", eventTypeIds);
 
         return "events/eventList";
+    }
+
+    @GetMapping("/{id}")
+    public String getEventById(@PathVariable("id") Long eventId, Model model,
+                               RedirectAttributes redirectAttributes) {
+        Optional<Event> event = eventService.getEventById(eventId);
+        if (event.isEmpty()) {
+            redirectAttributes.addFlashAttribute("error", "Данное мероприятие не было найдено в системе!");
+            return "redirect:/events";
+        }
+        model.addAttribute("event",event.get());
+        return "events/eventPage";
     }
 }
